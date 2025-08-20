101982c322a74dbc97e12a23e889173726494212
jiangjiang1118 <2844059184@qq.com>
2025-08-20 14:03:56 +0800

    好吧，必须填写更改信息

--- opencv/win.py
+++ opencv/win.py
@@ -1,5 +1,5 @@
 import cv2
-
+#cv2.namedWindow('new',cv2.WINDOW_AUTOSIZE)#cv2.WINDOW_AUTOSIZE是指窗口大小根据图像大小自动调整
 cv2.namedWindow('new',cv2.WINDOW_NORMAL)#cv2.WINDOW_NORMAL是指窗口大小根据用户设置的大小调整,可以resize
 cv2.resizeWindow('new',480,480)#设置窗口大小为640x480
 cv2.imshow('new',0)#这里的0是指窗口中显示的图像，可以是任意的图像，这里设置为0表示显示黑色图像
